eab870cfb3da395983baa27aaad48b3cbd38bf69
Akash Ratheesh <akashratheesh@gmail.com>
2022-04-11 19:03:24 +0000

    Added data download specification

--- SchemaBuilder.py
+++ SchemaBuilder.py
@@ -2,22 +2,30 @@ import numpy as np
 from schemaorg.main.parse import RecipeParser
 from schemaorg.main import Schema
 from schemaorg.templates.google import make_dataset
-
-default_properties = {
-        "about": "Test AMGeO Schema",
-        "citation": "Matsuo, T., Kilcommons, L. M., Ruohoniemi, J. M., and Anderson, B. J., “Assimilative Mapping of Geospace Observations (AMGeO): Data Science Tools for Collaborative Geospace Systems Science”, vol. 2019, 2019.",
-        "url": "https://amgeo.colorado.edu",
-        "name": "AMGeO Result"
-}
+import yaml
 
 
 class SchemaBuilder:
     def __init__(self,data=None):
         self.schema = Schema("Dataset")
+
+        with open("default_properties.yml", "r") as file:
+            default_properties = yaml.safe_load(file)
+        
+        author = default_properties['author']
+        del default_properties['author']
+        
+        authorSchema = Schema("Organization");
+
+        for authorprop in author:
+            authorSchema.add_property(authorprop,author[authorprop])
+        
+        self.add_property("author",authorSchema)
         self.add_property_set(default_properties)
         self.data = data
         if(data):
             dataTime = str(data.coords['time'].values[0])
+            dataTime = dataTime[:dataTime.index('.')]
             self.add_property("temporalCoverage",dataTime)
             
             
@@ -35,30 +43,35 @@ class SchemaBuilder:
             dumSpatialMax.add_property("latitude",maxLat)
             dumSpatialMax.add_property("longitude",maxLon)
 
-            
-            
-
             self.add_property("name",data.attrs['description']+ " for " + dataTime)
             self.add_property("version",data.attrs['version'])
-            self.add_property("about",data.attrs['description'])
+            self.add_property("description",data.attrs['description'])
             self.add_property("spatialCoverage",[dumSpatialMin,dumSpatialMax])
             variable_list =  list(data.keys())
             
             variable_Schema = []
             for variable in variable_list:
                 dumS = Schema("PropertyValue");
-                dumS.add_property("name",variable)
+                dumS.add_property("name",data.data_vars.get(variable).attrs['longname'])
+                dumS.add_property("unitText",data.data_vars.get(variable).attrs['units'])
                 variable_Schema.append(dumS)
             
             self.add_property("variableMeasured",variable_Schema)
 
     def add_property(self,name,value):
         self.schema.add_property(name,value)
-
+    
+    def add_url(self,siteurl,dataurl):
+        self.schema.add_property("url",siteurl)
+        distribution = Schema("DataDownload")
+        distribution.add_property("contentUrl",dataurl)
+        distribution.add_property("encodingFormat","application/x-netcdf")
+        self.add_property("distribution",distribution)
+       
     def add_property_set(self,property_set):
         for key,value in property_set.items():
             self.add_property(key,value)
     
     def to_html(self,name="index.html",template="google/dataset-table.html"):
-        dataset = make_dataset(self.schema, name,template=template)
+        dataset = make_dataset(self.schema, name,template =template)
         return dataset
---
+++ schemaGenerator.py
@@ -0,0 +1,39 @@
+import os
+import xarray
+from SchemaBuilder import *
+import xml.etree.cElementTree as ET
+from datetime import date
+
+
+
+def autoSchemaGenerator(rootName,folder,urlBase):
+    files = []
+    
+    
+    root = ET.Element("urlset", xmlns="http://www.sitemaps.org/schemas/sitemap/0.9")
+    today = date.today().strftime("%Y-%m-%d")
+    
+    for file in os.listdir(folder):
+        if file.endswith(".nc"):
+            files.append(file)
+
+    for file in files:
+        currdata = xarray.open_dataset(folder+"/"+file)
+        schema = SchemaBuilder(data=currdata)
+        schema.add_url( urlBase+"/static/data/"+file[:file.index('.nc')]+".html"
+                       ,urlBase+"/static/data/"+file)
+        html = schema.to_html(template="google/visual-dataset.html")
+        filew = open(folder+"/data-"+file[:file.index('.nc')]+".html","w")
+        filew.write(html)
+        filew.close()
+       
+        doc = ET.SubElement(root, "url")
+        
+        ET.SubElement(doc, "loc").text = urlBase+"/static/data/"+file[:file.index('.nc')]+".html"
+        ET.SubElement(doc, "lastmod").text = today
+        
+
+
+    tree = ET.ElementTree(root)
+    ET.indent(tree, space="\t", level=0)
+    tree.write( rootName+"/sitemap.xml",encoding = "UTF-8", xml_declaration = True)
